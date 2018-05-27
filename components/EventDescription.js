35b343cb0d0dfc02a0ef644337f7ce29348adcda
sawirricardo <sawir.ricardo@gmail.com>
2018-05-28 02:06:12 +0700

    added html parser

--- components/EventDescription.js
+++ components/EventDescription.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Header, Container } from 'semantic-ui-react';
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+  htmlparser2
+} from "react-html-parser";
 
 
 const EventDescription = (props) => {
     const { description } = props;
-    
+
     return (
         <React.Fragment>
             <Container text>
                 <Header as="h3">About this Event</Header>
-                {description}
+                {ReactHtmlParser(description)}
             </Container>
         </React.Fragment>
     )
